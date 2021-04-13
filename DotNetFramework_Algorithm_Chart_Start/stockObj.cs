0361a7c8f98adaa2720978b2a597f0451e8dbc95
vmille25 <72162006+vmille25@users.noreply.github.com>
2021-04-13 12:38:10 -0400

    Adding the refactored classes to the project.

---
+++ DotNetFramework_Algorithm_Chart_Start/stockObj.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetFramework_Algorithm_Chart_Start
+{
+    // stockObj class for holding the specific information of a ticker symbol when a pattern is found
+    public class stockObj
+    {
+        public int id { get; set; }
+        public string symbol { get; set; }
+        public string startdate { get; set; }
+        public string enddate { get; set; }
+        public int interval { get; set; }
+        public string name { get; set; }
+        public string image { get; set; }
+    }
+}
